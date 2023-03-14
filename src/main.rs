22ece17ea5d0422b2d16b0ccb56b85294c3e23d8
sarthhh <shiva02939@gmail.com>
2023-03-14 07:41:05 +0530

    init

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
+use rocket::fs::{FileServer, NamedFile};
+
 #[macro_use]
 extern crate rocket;
-
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, from Rocket!"
+async fn index() -> NamedFile {
+    NamedFile::open("static/templates/home.html")
+        .await
+        .expect("Unable to find file.")
+}
+
+#[get("/paste")]
+async fn paste() -> NamedFile {
+    NamedFile::open("static/templates/paste.html")
+        .await
+        .expect("Unable to find file.")
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index, paste])
+        .mount("/static", FileServer::from("static"))
 }
