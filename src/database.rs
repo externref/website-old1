6604d2b1bf9e4eff7fddb70f36e38bfafa1d67b3
sarthhh <shiva02939@gmail.com>
2023-03-15 09:49:50 +0530

    setup paste viewer

--- src/database.rs
+++ src/database.rs
@@ -1,7 +1,8 @@
+use rand;
 use sqlx;
 
 pub struct DatabaseHandler {
-    pub pool: sqlx::PgPool,
+    pub pool: std::sync::Arc<sqlx::PgPool>,
 }
 
 impl DatabaseHandler {
@@ -14,9 +15,31 @@ impl DatabaseHandler {
             );
         ",
         )
-        .execute(&self.pool)
+        .execute(&*self.pool)
         .await
         .expect("Unable to create database.");
         return self;
+    }
+    fn create_random_string(&mut self) -> String {
+        let s: String =
+            rand::Rng::sample_iter(rand::thread_rng(), &rand::distributions::Alphanumeric)
+                .take(7)
+                .map(char::from)
+                .collect();
+        return s;
+    }
+    pub async fn add_paste(&mut self, content: &str) -> String {
+        let paste_id = self.create_random_string();
+        sqlx::query(&format!(
+            "
+                INSERT INTO pastes 
+                VALUES ({}, {})
+                ",
+            paste_id, content
+        ))
+        .execute(&*self.pool)
+        .await
+        .expect("Unable to add new paste.");
+        return paste_id;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 mod database;
+
 use rocket::fs::{FileServer, NamedFile};
 use sqlx;
-
-struct DBInjection {
-    database: database::DatabaseHandler,
-}
+use std::sync::Arc;
 
 #[macro_use]
 extern crate rocket;
+
+struct DBInjection {
+    pool: Arc<sqlx::PgPool>,
+}
+
 #[get("/")]
 async fn index() -> NamedFile {
     NamedFile::open("static/templates/home.html")
@@ -22,24 +25,37 @@ async fn paste() -> NamedFile {
         .expect("Unable to find file.")
 }
 
+#[get("/paste?<id>")]
+async fn view_paste(id: &str) -> NamedFile {
+    NamedFile::open("static/templates/view_paste.html")
+        .await
+        .expect("Unable to find file.")
+}
+
 #[get("/create_paste?<paste_data>")]
-async fn create_paste(paste_data: &str) {
-    println!("{}", paste_data)
+async fn create_paste(
+    paste_data: &str,
+    injection: &rocket::State<DBInjection>,
+) -> rocket::response::Redirect {
+    let mut handler = database::DatabaseHandler {
+        pool: injection.pool.clone(),
+    };
+    let paste_id = handler.add_paste(paste_data).await;
+    rocket::response::Redirect::to(format!("./paste?id={}", paste_id))
 }
 
 #[launch]
 async fn rocket() -> _ {
-    let pool = sqlx::PgPool::connect(&std::env::var("PGSQL_URL").expect("PGSQL_URL key not found."))
-        .await
-        .expect("Unable to create database pool connection.");
-    let db_handler: database::DatabaseHandler =
-        database::DatabaseHandler { pool: pool }.setup().await;
-    let injection: DBInjection = DBInjection {
-        database: db_handler,
-    };
+    let pool = Arc::new(
+        sqlx::PgPool::connect(&std::env::var("PGSQL_URL").expect("PGSQL_URL key not found."))
+            .await
+            .expect("Unable to create database pool connection."),
+    );
+
+    let injection = DBInjection { pool: pool.clone() };
 
     rocket::build()
-        .mount("/", routes![index, paste])
+        .mount("/", routes![index, paste, view_paste])
         .mount("/paste", routes![create_paste])
         .mount("/static", FileServer::from("././static"))
         .manage(injection)
